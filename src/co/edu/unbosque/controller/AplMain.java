aa7c8d871d166337b73bee7509009d3f3d6e5f1e
Ricardo759486 <jrcuevas@unbosque.edu.co>
2021-09-30 12:32:35 -0500

    Packages

---
+++ src/co/edu/unbosque/controller/AplMain.java
@@ -0,0 +1,18 @@
+package co.edu.unbosque.controller;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class AplMain {
+
+	public static void main(String[] args) throws Exception {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (UnsupportedLookAndFeelException e) {
+		} catch (ClassNotFoundException e) {
+		} catch (InstantiationException e) {
+		} catch (IllegalAccessException e) {
+		}
+		// Controller control = new Controller();
+	}
+}
