244fc5ecfd6feb317994a3eca85959ce8297cb31
JuanaTorres <jvtorresp@unbosque.edu.co>
2021-09-30 16:19:03 -0500

    Clase Controller

---
+++ src/co/edu/unbosque/controller/Controller.java
@@ -0,0 +1,24 @@
+package co.edu.unbosque.controller;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import co.edu.unbosque.view.View;
+import co.edu.unbosque.model.*;
+
+public class Controller implements ActionListener {
+	
+	private View principal;
+	private Algoritmo algoritmo;
+	
+	public Controller() {
+		principal= new View();
+		algoritmo= new Algoritmo();
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
