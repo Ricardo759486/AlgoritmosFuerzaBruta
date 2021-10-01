5846c17c4b210011b0175f97d18b92ec327bc73f
RobinsonF <rgutierrezs@unbosque.edu.co>
2021-09-30 19:45:21 -0500

    Prueba

---
+++ Libraries/AplMain.java
@@ -0,0 +1,23 @@
+package co.edu.unbosque.controller;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import com.jtattoo.plaf.hifi.HiFiLookAndFeel;
+
+
+
+public class AplMain {
+
+	public static void main(String[] args) throws Exception {
+		try {
+			UIManager.setLookAndFeel(HiFiLookAndFeel.class.getName());
+
+		} catch (UnsupportedLookAndFeelException e) {
+		} catch (ClassNotFoundException e) {
+		} catch (InstantiationException e) {
+		} catch (IllegalAccessException e) {
+		}
+//		Controller control = new Controller();
+	}
+}
