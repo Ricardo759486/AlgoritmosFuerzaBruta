02b6bd58f2209d9fb03b627d433490b7d82ef162
Ricardo759486 <jrcuevas@unbosque.edu.co>
2021-10-05 15:35:30 -0500

    Controller

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -41,43 +41,36 @@ public class Controller implements ActionListener {
 			if (inputs[0].equals("0")) {
 				if(view.getSelectionPanel().getCheckSearch().isSelected() == false) {
 					String text = world.getAlFile().toLowerCase();
-					view.getShowPanel().getTxtAreaShow().setText(world.getAlFile());
-					view.getShowPanel().getLabelResult().setText( inputs[1] + world.search(inputs[1].toLowerCase(), inputs[2]));
-					if (inputs[2].equals("BM")) {
-						String[] array = world.getAlgorithm().orBM(world.getAlFile().toLowerCase(), inputs[1].toLowerCase())
-								.split(",");
-						view.getShowPanel().searchText(array, inputs[1].length());
-					} else if (inputs[2].equals("KMP")) {
-						String[] array = world.getAlgorithm().orKMP(world.getAlFile().toLowerCase(), inputs[1].toLowerCase())
-								.split(",");
-						view.getShowPanel().searchText(array, inputs[1].length());
-					}
-
-					view.getSplitPane().setRightComponent(view.getShowPanel());
+					this.prueba(text, inputs[1].toLowerCase(), inputs[2].toLowerCase());
 				}else {
-					String text = world.getAlFile().toLowerCase();
-					view.getShowPanel().getTxtAreaShow().setText(world.getAlFile());
-					view.getShowPanel().getLabelResult().setText( inputs[1] + world.search2(inputs[1], inputs[2]));
-					if (inputs[2].equals("BM")) {
-						String[] array = world.getAlgorithm().orBM(world.getAlFile(), inputs[1])
-								.split(",");
-						view.getShowPanel().searchText(array, inputs[1].length());
-					} else if (inputs[2].equals("KMP")) {
-						String[] array = world.getAlgorithm().orKMP(world.getAlFile(), inputs[1])
-								.split(",");
-						view.getShowPanel().searchText(array, inputs[1].length());
-					}
-
-					view.getSplitPane().setRightComponent(view.getShowPanel());
+					String text = world.getAlFile();
+					this.prueba(text, inputs[1], inputs[2]);
 				}
-				
 			} else {
 				view.showWarningMessage(inputs[1]);
 			}
 		} else {
 			view.showWarningMessage("First you must select the .txt file");
 		}
+	}
+	
+	public void prueba(String text, String inputs1, String inputs2) {
+		
+		view.getShowPanel().getTxtAreaShow().setText(world.getAlFile());
+		view.getShowPanel().getLabelResult().setText( inputs1 + world.search(inputs1, inputs2));
+		if (inputs2.equals("BM")) {
+			String[] array = world.getAlgorithm().orBM(world.getAlFile(), inputs1)
+					.split(",");
+			view.getShowPanel().searchText(array, inputs1.length());
+		} else if (inputs2.equals("KMP")) {
+			String[] array = world.getAlgorithm().orKMP(world.getAlFile(), inputs1)
+					.split(",");
+			view.getShowPanel().searchText(array, inputs1.length());
+		}
 
+		view.getSplitPane().setRightComponent(view.getShowPanel());
+		
+		
 	}
 
 }
